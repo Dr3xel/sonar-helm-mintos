f27d421af3a5dfc8e12a4a83842c996857bc3dcc
Dr3xel <dimamiftahov96@gmail.com>
2026-06-24 21:57:05 +0300

    terraform fmt fix applied

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -2,8 +2,8 @@ resource "helm_release" "postgresql" {
   name      = "postgresql"
   namespace = kubernetes_namespace.sonarqube.metadata[0].name
 
-  chart = "${path.module}/../helm/postgresql"
-  lint = true
+  chart            = "${path.module}/../helm/postgresql"
+  lint             = true
   create_namespace = false
 
   wait            = true
@@ -16,8 +16,8 @@ resource "helm_release" "sonarqube" {
   name      = "sonarqube"
   namespace = kubernetes_namespace.sonarqube.metadata[0].name
 
-  chart = "${path.module}/../helm/sonarqube"
-  lint = true
+  chart            = "${path.module}/../helm/sonarqube"
+  lint             = true
   create_namespace = false
 
   wait            = true
